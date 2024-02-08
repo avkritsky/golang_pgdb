3d4c3a7266364a3a71db15bf51aa230b5ee9f69a
Anatoly Kritskiy <79113172923@ya.ru>
2024-02-08 16:50:30 +0300

    fit: add new funcs

--- db_repo.go
+++ db_repo.go
@@ -123,5 +123,106 @@ func AddUser(d Userdata) int {
 		return userID
 	}
 
-	
+	insertStatement = `insert into "userdata" ("user_id", "name", "surname", "description") values ($1, $2, $3, $4)`
+
+	_, err = db.Exec(insertStatement, userID, d.Name, d.Surname, d.Description)
+
+	if err != nil {
+		fmt.Println("Error with insert #2:", err)
+		return -1
+	}
+
+	return userID
+
+}
+
+func DeleteUser(id int) error {
+	db, err := openConnection()
+
+	if err != nil {
+		fmt.Println("Connection error:", err)
+		return err
+	}
+
+	defer db.Close()
+
+	checkQuery := `SELECT "username" FROM users WHERE "id" = $1`
+
+	rows, err := db.Query(checkQuery, id)
+
+	if err != nil {
+		fmt.Println("Error check:", err)
+		return err
+	}
+
+	var username string
+
+	for rows.Next() {
+		err = rows.Scan(&username)
+
+		if err != nil {
+			return err
+		}
+	}
+	defer rows.Close()
+
+	if exists(username) != id {
+		return fmt.Errorf("User with id #%d does not exist!\n", id)
+	}
+
+	deleteStatement := `DELETE FROM "users" WHERE "id" = $1`
+	_, err = db.Exec(deleteStatement, id)
+
+	if err != nil {
+		return err
+	}
+
+	deleteStatement = `DELETE FROM "userdata" WHERE "userid" = $1`
+	_, err = db.Exec(deleteStatement, id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ListUser() ([]Userdata, error) {
+	db, err := openConnection()
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer db.Close()
+
+	data := []Userdata{}
+
+	rows, err := db.Query(`SELECT "id", username, name, surname, description  FROM "users", "userdata" WHERE users.id = userdata.userid`)
+
+	for rows.Next() {
+		var id int
+		var (
+			username string
+	        name string
+			surname string
+			description string
+		)
+
+		err = rows.Scan(&id, &username, &name, &surname, &description)
+
+		if err != nil {
+			return data, err
+		}
+
+		temp := Userdata{
+			id, username, name, surname, description,
+		}
+
+		data = append(data, temp)
+	}
+
+	defer rows.Close()
+
+	return data, nil
 }
